3dfe8fc762abbc4b6a74f2ad3344fcf8d1be15de
Enes Yağmur <enesyagmur99@gmail.com>
2024-05-07 13:18:22 +0300

    sırayla x ve o gösterimi yapıldı sırada oyunun bitme ve kazanma kontrolü var

--- src/games/xox/Box.jsx
+++ src/games/xox/Box.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import x from "../../assets/cross.png";
+import o from "../../assets/circle.png";
+import "./box.scss";
 
-const Box = () => {
-  return <div>Box</div>;
+const Box = ({
+  value,
+  setValue,
+  resetCheck,
+  setResetCheck,
+  id,
+  game,
+  setGame,
+}) => {
+  const [thisValue, setThisValue] = useState();
+  const newArray = game;
+
+  const changeValue = () => {
+    setResetCheck("");
+    if (value === "") {
+      setThisValue("x");
+      newArray[id].value = "x";
+      setValue("o");
+    } else if (value === "x") {
+      setThisValue("x");
+      newArray[id].value = "x";
+      setValue("o");
+    } else if (value === "o") {
+      setThisValue("o");
+      newArray[id].value = "o";
+      setValue("x");
+    }
+    setGame(newArray);
+  };
+
+  useEffect(() => {
+    if (resetCheck === "reset") {
+      setThisValue("");
+      setValue("");
+    }
+  }, [resetCheck]);
+
+  if (thisValue) {
+    return (
+      <div className="box">
+        {thisValue === "x" ? <img src={x} alt="" /> : <img src={o} alt="" />}
+        {<p>{id}</p>}
+      </div>
+    );
+  } else {
+    return <div className="box" onClick={changeValue}></div>;
+  }
 };
 
 export default Box;
--- src/games/xox/Xox.jsx
+++ src/games/xox/Xox.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./xox.scss";
+import Box from "./Box";
+import gamePlan from "./gameTable.json";
 
 const Xox = () => {
-  return <div className="xox">Xox</div>;
+  const [value, setValue] = useState("");
+  const [resetCheck, setResetCheck] = useState("");
+  const [game, setGame] = useState(gamePlan);
+  // şu an oyunun bitimini kontrol etmeye çalışıyorum
+  useEffect(() => {
+    const result = game.filter(
+      (item) => item.value === "x" || item.value === "o"
+    );
+  }, [game]);
+  console.log(game);
+  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  return (
+    <div className="xox">
+      <main>
+        {array.map((i) => (
+          <Box
+            value={value}
+            setValue={setValue}
+            resetCheck={resetCheck}
+            setResetCheck={setResetCheck}
+            game={game}
+            setGame={setGame}
+            key={i}
+            id={i}
+          />
+        ))}
+      </main>
+      <button className="reset" onClick={() => setResetCheck("reset")}>
+        RESTART
+      </button>
+    </div>
+  );
 };
 
 export default Xox;
